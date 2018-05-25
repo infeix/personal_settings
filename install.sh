fa3e07c1e6b0dd8c0f138b46d46b66a77c74f2fe
Johannes Kroop <johkroop@gmail.com>
2018-05-25 15:14:17 +0200

    istall Terminology and copy_files.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 sudo apt-get install git curl powerline htop gitk firefox
 
 ## Install Neo Vim with some plugin
+
 sudo apt-get install neovim git
 
 mkdir tmp
@@ -12,30 +13,35 @@ sh ./installer.sh ~/.cache/dein
 cd ..
 rm -rf tmp
 
+## Install Terminology
+
+sudo add-apt-repository ppa:enlightenment-git/ppa
+sudo apt-get update
+sudo apt-get install terminology
+
 ## Install Chrome
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update 
 sudo apt-get install google-chrome-stable
 
 ## Install Slack
+
 sudo snap install slack --classic
 
 ## Install Sublime Text
+
 sudo snap install sublime-text --classic
 
 ## Install keepass
+
 sudo add-apt-repository ppa:jtaylor/keepass
 sudo apt-get update
 sudo apt-get install keepass2
 
 ## Install zsh
+
 sudo apt-get install zsh
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-cp -ru . ~
-rm ~/terminal_settings.png
-rm ~/install.sh
-rm ~/README.md
 
