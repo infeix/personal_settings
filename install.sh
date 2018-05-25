fad9029223571c5eafcf7a4d1375bf6be0efce2b
Johannes Kroop <johkroop@gmail.com>
2018-05-25 15:02:20 +0200

    bug caused by moving in tmp folder

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ sudo apt-get install git curl powerline htop gitk firefox
 ## Install Neo Vim with some plugin
 sudo apt-get install neovim git
 
-mkdir ~/tmp
-cd ~/tmp
+mkdir tmp
+cd tmp
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh ~/.cache/dein
+cd ..
+rm tmp
 
 ## Install Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
